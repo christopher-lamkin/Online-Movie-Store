7088c414ee3538f00ccfaf214664ee5ff65efd48
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-24 08:22:08 -0800

    create a movie model and migration

--- moviestore/db/migrate/20170224161750_create_movies.rb
+++ moviestore/db/migrate/20170224161750_create_movies.rb
@@ -2,6 +2,13 @@ class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
 
+      t.string :title
+      t.integer :release_year
+      t.price :float
+      t.text :description
+      t.integer :imdb_id
+      t.string :poster_url
+
       t.timestamps
     end
   end
