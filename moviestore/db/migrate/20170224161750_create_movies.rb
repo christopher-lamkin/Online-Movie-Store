698344750ae4427951e302590d68637ea0a67b59
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-24 09:36:41 -0800

    add csv file with list of movies

--- moviestore/db/migrate/20170224161750_create_movies.rb
+++ moviestore/db/migrate/20170224161750_create_movies.rb
@@ -4,9 +4,9 @@ class CreateMovies < ActiveRecord::Migration[5.0]
 
       t.string :title
       t.integer :release_year
-      t.price :float
+      t.float :price
       t.text :description
-      t.integer :imdb_id
+      t.string :imdb_id
       t.string :poster_url
 
       t.timestamps
